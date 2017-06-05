7799ffd8fc5a9d94982c3d2efe3d49a0bdf352aa
Naomi Urnes <naomiurnes@gmail.com>
2017-06-05 14:03:40 -0400

    Can submit new color for h1

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const personForm = document.querySelector('#mine')
 function handle(event){
     event.preventDefault()
     const form = event.target
-    console.log('SUBMIT!' + form.personName.value)
+    console.log(event.target)
+    console.log('SUBMIT!' + form.nameColor.value)
     const name = form.personName.value
+    const color = form.nameColor.value
     document.querySelector('h1').textContent = name
+    document.querySelector('h1').style = "color:" + color
 }
-personForm.addEventListener("submit", handle)
+personForm.addEventListener('submit', handle)
